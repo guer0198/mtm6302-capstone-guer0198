fab4cc243daeea4c8a5b52f3125badca5ccf0bf6
Enrico Andrade <guer0198@algonquinlive.com>
2023-04-13 18:03:49 -0400

    creates script

---
+++ script.js
@@ -0,0 +1,49 @@
+ const question = [
+    {
+        question: "Who won the 2018 FIFA World Cup?",
+        answers: [
+            {text: "Brazil", correct: false},
+            {text: "France", correct: true},
+            {text: "Croatia", correct: false},
+            {text: "Belgium", correct: false},
+        ]
+    },
+    {
+        question: "Who did Zinedine Zidane headbutt in the 2006 final?",
+        answers: [
+            {text: "Materazzi", correct: true},
+            {text: "Ronaldo", correct: false},
+            {text: "Messi", correct: false},
+            {text: "Cannavaro", correct: false},
+        ]
+    },
+    {
+        question: "What was the first World Cup trophy called?",
+        answers: [
+            {text: "Joseph Blater", correct: false},
+            {text: "Puskas", correct: false},
+            {text: "Jose Bernabeu", correct: false},
+            {text: "Jules Rimet", correct: true},
+        ]
+    },
+    {
+        question: "In what year did Spain win their first World Cup?",
+        answers: [
+            {text: "2014", correct: false},
+            {text: "2010", correct: true},
+            {text: "2006", correct: false},
+            {text: "1994", correct: false},
+        ]
+    },
+    {
+        question: "Who was the top scorer in the 2002 FIFA World Cup?",
+        answers: [
+            {text: "Klose", correct: false},
+            {text: "Zidane", correct: false},
+            {text: "Ronaldo Nazario", correct: true},
+            {text: "Ronaldinho Gaucho", correct: false},
+        ]
+    },
+ ];
+
+ const questionTab = document.getElementById("question");
