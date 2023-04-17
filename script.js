e6e1934aa8f09c115ca62d2b7fdd36991759b191
Enrico Andrade <guer0198@algonquinlive.com>
2023-04-16 22:15:40 -0400

    corrects NBA quiz

--- script.js
+++ script.js
@@ -100,8 +100,8 @@ const nba = [
 
 const nfl = [
     {
-        question: "When was the NFL founded?",
-        answers: [
+        questionNfl: "When was the NFL founded?",
+        answersNfl: [
             {text: "1935", correct: false},
             {text: "1920", correct: true},
             {text: "1950", correct: false},
@@ -109,8 +109,8 @@ const nfl = [
         ]
     },
     {
-        question: "Who won the first Super Bowl?",
-        answers: [
+        questionNfl: "Who won the first Super Bowl?",
+        answersNfl: [
             {text: "Kansas City Chiefs", correct: false},
             {text: "Oakland Raiders", correct: false},
             {text: "Green Bay Packers", correct: true},
@@ -118,8 +118,8 @@ const nfl = [
         ]
     },
     {
-        question: "Who is the youngest player in NFL history?",
-        answers: [
+        questionNfl: "Who is the youngest player in NFL history?",
+        answersNfl: [
             {text: "Richard Sligh", correct: false},
             {text: "William The Refrigerator", correct: false},
             {text: "Brett Favre", correct: false},
@@ -127,8 +127,8 @@ const nfl = [
         ]
     },
     {
-        question: "Who has the most receiving yards in a single game?",
-        answers: [
+        questionNfl: "Who has the most receiving yards in a single game?",
+        answersNfl: [
             {text: "Floyd Little", correct: true},
             {text: "Kurt Warner", correct: false},
             {text: "Joe Kapp", correct: false},
@@ -136,8 +136,8 @@ const nfl = [
         ]
     },
     {
-        question: "Who has the most interceptions in a single season?",
-        answers: [
+        questionNfl: "Who has the most interceptions in a single season?",
+        answersNfl: [
             {text: "Eric Dickerson", correct: false},
             {text: "Michael Strahan", correct: false},
             {text: "Cristiano Ronaldo", correct: false},
@@ -233,31 +233,18 @@ const nfl = [
 
  // modal element
 let modal = document.getElementById("modal");
-let modalNba = document.getElementById("modalNba");
 let btnModal = document.getElementById("soccer");
-let btnModalNba = document.getElementById("nba");
-let btnModalNfl = document.getElementById("nfl");
 let span = document.getElementsByClassName("close")[0];
-let spanNba = document.getElementsByClassName("closeNba")[0];
 
 btnModal.onclick = function() {
   modal.style.display = "block";
 }
 
-btnModalNba.onclick = function() {
-    modalNba.style.display = "block";
-}
-btnModalNfl.onclick = function() {
-    modalNfl.style.display = "block";
-}
 
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-spanNba.onclick = function() {
-    modalNba.style.display = "none";
-}
 
 // NBA QUIZ
 
@@ -283,41 +270,41 @@ const nextButtonNba = document.getElementById("next-btnNba");
     questionElementNba.innerHTML = questionNoNba + ". " + currentQuestionNba.questionNba;
 
     currentQuestionNba.answersNba.forEach(answer => {
-        const buttonNba = document.createElement("buttonNba");
+        const buttonNba = document.createElement("button");
         buttonNba.innerHTML = answer.text;
         buttonNba.classList.add("btnNba");
         answerButtonsNba.appendChild(buttonNba);
         if(answer.correct){
             buttonNba.dataset.correct = answer.correct;
         }
-        buttonNba.addEventListener("click", selectAnswer);
+        buttonNba.addEventListener("click", selectAnswerNba);
     });
  }
 
  function resetStateNba(){
     nextButtonNba.style.display = "none";
-    while(answerButtonsNba.firstElementChild){
+    while(answerButtonsNba.firstChild){
         answerButtonsNba.removeChild(answerButtonsNba.firstChild);
     }
  }
 
  function selectAnswerNba(e){
     const selectedBtnNba = e.target;
-    const isCorrect = selectedBtnNba.dataset.correct === "true";
-    if(isCorrect){
+    const isCorrectNba = selectedBtnNba.dataset.correct === "true";
+    if(isCorrectNba){
         selectedBtnNba.classList.add("correct");
         scoreNba++;
     }else{
         selectedBtnNba.classList.add("incorrect");
     }
 
-    Array.from(answerButtonsNba.children).forEach(buttonNba => {
-        if(buttonNba.dataset.correct === "true"){
-            buttonNba.classList.add("correct");
+    Array.from(answerButtonsNba.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
         }
-        buttonNba.disabled = true;
+        button.disabled = true;
     });
-    nextButtonNba.style.display = "inline";
+    nextButtonNba.style.display = "block";
  }
 
  function showScoreNba(){
@@ -345,3 +332,118 @@ const nextButtonNba = document.getElementById("next-btnNba");
  })
 
  startQuizNba();
+
+
+let modalNba = document.getElementById("modalNba");
+let btnModalNba = document.getElementById("nba");
+let spanNba = document.getElementsByClassName("closeNba")[0];
+
+
+btnModalNba.onclick = function() {
+    modalNba.style.display = "block";
+}
+
+spanNba.onclick = function() {
+    modalNba.style.display = "none";
+}
+
+// NFL QUIZ
+
+const questionElementNfl = document.getElementById("questionNfl");
+const answerButtonsNfl = document.getElementById("answer-buttonsNfl");
+const nextButtonNfl = document.getElementById("next-btnNfl");
+
+
+ let currentQuestionIndexNfl = 0;
+ let scoreNfl = 0;
+
+ function startQuizNfl(){
+    currentQuestionIndexNfl = 0;
+    scoreNfl = 0;
+    nextButtonNfl.innerHTML = "Next";
+    showQuestionNfl();
+ }
+
+ function showQuestionNfl(){
+    resetStateNfl();
+    let currentQuestionNfl = nfl[currentQuestionIndexNfl];
+    let questionNoNfl = currentQuestionIndexNfl + 1;
+    questionElementNfl.innerHTML = questionNoNfl + ". " + currentQuestionNfl.questionNfl;
+
+    currentQuestionNfl.answersNfl.forEach(answer => {
+        const buttonNfl = document.createElement("button");
+        buttonNba.innerHTML = answer.text;
+        buttonNba.classList.add("btnNfl");
+        answerButtonsNfl.appendChild(buttonNfl);
+        if(answer.correct){
+            buttonNfl.dataset.correct = answer.correct;
+        }
+        buttonNfl.addEventListener("click", selectAnswerNfl);
+    });
+ }
+
+ function resetStateNfl(){
+    nextButtonNfl.style.display = "none";
+    while(answerButtonsNfl.firstChild){
+        answerButtonsNfl.removeChild(answerButtonsNfl.firstChild);
+    }
+ }
+
+ function selectAnswerNfl(e){
+    const selectedBtnNfl = e.target;
+    const isCorrectNfl = selectedBtnNfl.dataset.correct === "true";
+    if(isCorrectNfl){
+        selectedBtnNfl.classList.add("correct");
+        scoreNfl++;
+    }else{
+        selectedBtnNfl.classList.add("incorrect");
+    }
+
+    Array.from(answerButtonsNfl.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButtonNfl.style.display = "block";
+ }
+
+ function showScoreNfl(){
+    resetStateNfl();
+    questionElementNfl.innerHTML = `You scored ${scoreNfl} out of ${nfl.length}!`;
+    nextButtonNfl.innerHTML = "Play Again";
+    nextButtonNfl.style.display = "block";
+ }
+
+ function handleNextButtonNfl(){
+    currentQuestionIndexNfl++;
+    if(currentQuestionIndexNfl < nfl.length){
+        showQuestionNfl();
+    }else{
+        showScoreNfl();
+    }
+ }
+
+ nextButtonNfl.addEventListener("click", ()=>{
+    if(currentQuestionIndex < nfl.length){
+        handleNextButtonNfl();
+    }else{
+        startQuizNfl();
+    }
+ })
+
+ startQuizNfl();
+
+
+let modalNfl = document.getElementById("modalNfl");
+let btnModalNfl = document.getElementById("callnfl");
+let spanNfl = document.getElementsByClassName("closeNfl")[0];
+
+
+btnModalNfl.onclick = function() {
+    modalNfl.style.display = "block";
+}
+
+spanNfl.onclick = function() {
+    modalNfl.style.display = "none";
+}
